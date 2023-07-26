4c898f39f75bac524c5810af8f94cfa6e80f0f80
Devin Rogers <140612132+DevinRogers755@users.noreply.github.com>
2023-07-26 06:30:50 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {output: 'export', basePath: '/abamex'}
+const nextConfig = {output: 'export', basePath: '/abamex', images: {
+    loader: "akamai",
+    path: "/",
+  },
+}
 
 module.exports = nextConfig
